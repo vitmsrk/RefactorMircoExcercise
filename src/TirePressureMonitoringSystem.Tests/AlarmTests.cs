365037e9901b3e71c48b08b9027607834501f61f
Vitalii Mosoriuk <vitalii.mosoriuk@solarwinds.com>
2022-08-05 12:34:15 +0200

    Add first Alarm tests

---
+++ src/TirePressureMonitoringSystem.Tests/AlarmTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace TDDMicroExercises.TirePressureMonitoringSystem.Tests
+{
+    [TestFixture]
+    public class AlarmTests
+    {
+        [Test]
+        public void Check_DoesNotThrowAnyExceptions()
+        {
+            var alarm = new Alarm();
+            Assert.DoesNotThrow(() => alarm.Check());
+        }
+    }
+}
